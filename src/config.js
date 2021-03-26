dfd1b47a33cb540c4cebd6bcbe51de6d149d734b
lehmancurtis147 <lehmancurtis147@gmail.com>
2021-03-26 18:26:52 -0500

    still at it

--- src/config.js
+++ src/config.js
@@ -22,37 +22,37 @@ export default {
     RELEASE_VARIANTS: {
         teamWin: {
             description: "Official Team Win Recovery Project.",
+            url: "/android-webinstall/public/releases/files/",
         },
         orangeFox: {
-            description:
-                "Unofficial Orange Fox Recovery Project.",
+            description: "Unofficial Orange Fox Recovery Project.",
+            url: "/android-webinstall/public/releases/files/",
         },
         pitchBlack: {
-            description:
-                "Unofficial Pitch Black Recovery Project.",
+            description: "Unofficial Pitch Black Recovery Project.",
+            url: "/android-webinstall/public/releases/files/",
         },
         skyHawk: {
-            description:
-                "Unofficial Sky Hawk Recovery Project.",
+            description: "Unofficial Sky Hawk Recovery Project.",
+            url: "/android-webinstall/public/releases/files/",
         },
         dark: {
-            description:
-                "Unofficial Dark Recovery Project.",
+            description: "Unofficial Dark Recovery Project.",
+            url: "/android-webinstall/public/releases/files/",
         },
         batik: {
-            description:
-                "Unofficial Batik Recovery Project.",
+            description: "Unofficial Batik Recovery Project.",
+            url: "/android-webinstall/public/releases/files/",
         },
         nusantara: {
-            description:
-                "Unofficial Nusantara Recovery Project.",
+            description: "Unofficial Nusantara Recovery Project.",
+            url: "/android-webinstall/public/releases/files/",
         },
     },
     DONATION_LINKS: [
         {
             title: "Paypal Donation Link",
-            description:
-                "If you are able to, please consider making a donation through PayPal. This allows us to support this and other projects now and in the future.",
+            description: "If you are able to, please consider making a donation through PayPal. This allows us to support this and other projects now and in the future.",
             highlight: true,
             url: "https://paypal.me/PizzaG98",
             icon: "paypal",
--- vue.config.js
+++ vue.config.js
@@ -1,20 +1,16 @@
 module.exports = {
-  publicPath:
-    '\'\'',
-
-  transpileDependencies: ["vuetify"],
-
-  chainWebpack: (config) => {
-    config.plugin("html").tap((args) => {
-          args[0].title = `${process.env.VUE_APP_OS_NAME} android-webinstall`;
-          args[0].osName = process.env.VUE_APP_OS_NAME;
-          args[0].osDesc = process.env.VUE_APP_OS_DESC;
-          args[0].analyticsDomain = process.env.VUE_APP_SA_DOMAIN;
-          return args;
-       });
+    publicPath:
+        process.env.NODE_ENV === "production"
+            ? process.env.VUE_APP_PROD_URL
+            : "/",
+    transpileDependencies: ["vuetify"],
+    chainWebpack: (config) => {
+        config.plugin("html").tap((args) => {
+            args[0].title = `${process.env.VUE_APP_OS_NAME} android-webinstall`;
+            args[0].osName = process.env.VUE_APP_OS_NAME;
+            args[0].osDesc = process.env.VUE_APP_OS_DESC;
+            args[0].analyticsDomain = process.env.VUE_APP_SA_DOMAIN;
+            return args;
+        });
     },
-
-  pwa: {
-    name: 'android-webinstall'
-  }
 };
